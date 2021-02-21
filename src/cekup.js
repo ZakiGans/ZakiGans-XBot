ca7877e161877e53635d2d2a2c316e963357f9e5
ZakiAhsan <79066165+ZakiGans@users.noreply.github.com>
2021-02-21 19:02:36 +0700

    Add files via upload

---
+++ src/cekup.js
@@ -0,0 +1,23 @@
+const cekup = (prefix) => { 
+
+	return `
+	
+「 *UPDATING* 」
+
+SEMUANYA FITUR TERBARU
+MENEMUKAN BUG?
+KETIK *${prefix}laporbug*
+REQWEST FITUR?
+KETIK *${prefix}reqwest*
+
+
+*NOTE :*
+
+[❗]  *MADED BY ZAKI GANS*
+
+ 
+`
+
+}
+
+exports.cekup = cekup
---
+++ src/memobot.js
@@ -0,0 +1,15 @@
+const memobot = (prefix) => { 
+	return `
+	
+╭──「 *JEJAK BOT* 」
+┃
+├ X BOT TEAM = V1. 0.00.00
+┃
+╰─────────────────╯
+
+`
+
+}
+
+exports.memobot = memobot
+
